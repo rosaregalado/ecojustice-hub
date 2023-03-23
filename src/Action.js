373350ea616f16e03b52bf9331437775b5c885bb
Rosa Regalado <rosaregalado101@gmail.com>
2023-03-23 02:45:43 -0700

    updated third page: take action

--- src/Action.js
+++ src/Action.js
@@ -1,62 +1,93 @@
 import Nav from './Nav';
 
 function Action() {
+  
   return (
     <div className="Action bg-gray-100">
       {/* Nav */}
       <Nav />
-      <h1 className="text-2xl font-mono text-blue-800 mt-6 text-center">Ways to Take Action</h1>
-      <div className="m-7 p-3">
+      <h1 className="text-2xl font-mono text-blue-800 mt-6 text-center">Take Action</h1>
+      <div className="m-10 p-5">
         <p>Environmental justice issues have far-reaching and devastating impacts on marginalized communities 
           around the world, and it is imperative that we take action to address them. According to the World Health Organization, 
           around 12.6 million deaths per year are attributed to environmental risks such as air pollution, chemical exposure, and water contamination. 
           Additionally, studies have shown that communities of color and low-income communities are disproportionately affected by environmental hazards and have higher rates of environmental health problems. 
         </p>
         <br/>
-        <p>Taking action against environmental injustice is not only a matter of social and environmental justice, but it also has significant implications for public health and the well-being of our planet.</p>
+        <p className="text-red-600 font-bold">Taking action against environmental injustice is not only a matter of social and environmental justice, but it also has significant implications for public health and the well-being of our planet.</p>
       </div>
 
-      {/* <div className="m-7">
-        <h2 className="text-2xl font-bold mb-4 text-blue-800 text-center">Environmental Justice Campaigns</h2>
-        <ul className="ml-6">
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">The Climate Strike</h3>
-            <p>A global movement of young people demanding urgent action on climate change.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">The Sunrise Movement</h3>
-            <p>A youth-led movement advocating for political action on climate change and creating millions of good jobs in the process.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">350.org</h3>
-            <p>An international movement working to end the use of fossil fuels and build a just and sustainable world.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">Fridays for Future</h3>
-            <p>A global movement of students striking for urgent action on climate change and a just transition to a renewable energy economy.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">The People's Climate March</h3>
-            <p>A series of global marches demanding urgent action on climate change and environmental justice.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">Stop the Dakota Access Pipeline</h3>
-            <p>An indigenous-led movement working to stop the construction of the Dakota Access Pipeline, which threatens water sources and sacred lands.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">The Sierra Club</h3>
-            <p>A grassroots environmental organization working to protect wildlife and wild places, advance clean energy, and combat climate change.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">Greenpeace</h3>
-            <p>An international environmental organization campaigning for solutions to environmental issues such as climate change, deforestation, and ocean pollution.</p>
-          </li>
-          <li className="mb-4">
-            <h3 className="text-lg font-bold">Food & Water Watch</h3>
-            <p>An organization working to ensure the food, water, and fish we consume is safe, accessible, and sustainably produced.</p>
-          </li>
-        </ul>
-      </div> */}
+      <div className="grid grid-rows-2 my-10">
+        <div>
+          <h1 className="text-center text-2xl overline hover:underline">Make a Donation</h1>
+          <p className="m-5 text-center">Protecting our planet is a big job — it takes millions of people like you to bring solutions to life. Your support matters.</p>
+          <div class="mx-20">
+            <button type="submit" class=" px-4 py-2 bg-blue-900 text-white rounded-md hover:bg-blue-600">
+              Donate Now
+            </button>
+          </div>
+        </div>
+
+
+        <div className="flex flex-col items-center">
+          <h2 className="text-center text-2xl overline hover:underline">Get Involved</h2>
+          <p className="m-5 text-center">Use your voice where it counts. Speak up for the health and safety of the environment and people everywhere.</p>
+        </div>
+
+        <div className="grid grid-cols-2">
+          {/* card 1 */}
+          <div className="p-6 m-10 bg-blue-900 text-white rounded-lg shadow-md">
+            <h1 className="text-lg font-bold text-white mb-2">
+              Protect tigers by switching to forest-friendly products
+            </h1>
+            <p className="text-white font-extralight">
+              Pledge to do your part to protect forests and save wildlife by supporting products that have the Forest Stewardship CouncilTM (FSC®) label.
+            </p>
+            <button className="block w-70 mt-4 bg-white text-black font-semibold p-3 rounded hover:bg-blue-600">
+              Take Action
+            </button>
+          </div>
+
+          {/* card 2 */}
+          <div className="p-6 m-10 outline-double bg-white rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold text-gray-800 mb-2">
+              No plastic in nature by 2030            </h2>
+            <p className="text-gray-700">
+              Urge the world's leaders to act strongly and decisively in developing the full content of the treaty to end plastic pollution by 2024.
+            </p>
+            <button className="block w-70 mt-4 bg-blue-900 text-white font-semibold p-3 rounded hover:bg-blue-600">
+              Take Action
+            </button>
+          </div>
+
+          {/* card 3 */}
+          <div className="p-6 m-10 bg-white rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold text-gray-800 mb-2">
+              Use Sustainable Transportation
+            </h2>
+            <p className="text-gray-700">
+              Reduce your carbon footprint by walking, biking, or using public transportation whenever possible.
+            </p>
+            <button className="block w-70 mt-4 bg-blue-900 text-white font-semibold p-3 rounded hover:bg-blue-600">
+              Take Action
+            </button>
+          </div>
+
+          {/* card 4 */}
+          <div className="p-6 m-10 bg-white rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold text-gray-800 mb-2">
+              Plant Trees
+            </h2>
+            <p className="text-gray-700">
+              Plant trees in your community or support reforestation efforts around the world.
+            </p>
+            <button className="block w-70 mt-4 bg-blue-900 text-white font-semibold p-3 rounded hover:bg-blue-600">
+              Take Action
+            </button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
